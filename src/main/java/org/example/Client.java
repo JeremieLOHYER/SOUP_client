ea51cc12e3a0887a8c63b41b9b4fc81e1efaff9a
jeremie <jeremie84tr@gmail.com>
2024-03-23 17:33:21 +0100

    client actions

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -10,10 +10,10 @@ public class Client
 
     MusiqueReceiverPrx musiqueReceiver = null;
 
-    public void init() {
+    public void init(String serverAdress) {
         com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize();
         communicator.getProperties().setProperty("Ice.Default.Package", "com.zeroc.demos.Ice.minimal");
-        ObjectPrx proxy = communicator.stringToProxy("musiqueReceiver:default -h localhost -p 5000 -t 5000");
+        ObjectPrx proxy = communicator.stringToProxy("musiqueReceiver:default -h " + serverAdress + " -p 5000 -t 5000");
         musiqueReceiver = MusiqueReceiverPrx.checkedCast(proxy);
     }
     public static void main(String[] args)
@@ -22,16 +22,21 @@ public class Client
             @Override
             public void run() {
                 Client monClient = new Client();
-                monClient.init();
+                monClient.init("192.168.1.6");
                 //System.out.println(monClient.musiqueReceiver.getSongs());
                 monClient.musiqueReceiver.select("pop/chipichipi.mp3");
                 monClient.musiqueReceiver.play();
                 try {
                     Thread.sleep(15000);
+                    monClient.musiqueReceiver.pause();
+                    Thread.sleep(2000);
+                    monClient.musiqueReceiver.play();
+                    Thread.sleep(15000);
+                    monClient.musiqueReceiver.stop();
                 } catch (InterruptedException e) {
                     System.out.println("je peux pas dormir");
+                    monClient.musiqueReceiver.stop();
                 }
-                monClient.musiqueReceiver.stop();
             }
         }).start();
         new Thread(new Runnable() {
